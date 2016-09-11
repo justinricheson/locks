5e964e526431e89ba7374d5713bd014a404012e7
justinricheson <justinricheson@icloud.com>
2016-09-11 12:52:31 -0600

    Add cas lock

---
+++ Locks/CASLock.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+
+namespace Locks
+{
+    /// <summary>
+    /// Like the naive lock, but uses CAS instructions to provide atomicity, and correct behavior
+    /// </summary>
+    public class CASLock : ILock
+    {
+        private int _lock;
+
+        public void Request(int pid)
+        {
+            while (Interlocked.CompareExchange(ref _lock, 1, 0) != 0) ;
+        }
+
+        public void Release(int pid)
+        {
+            _lock = 0;
+        }
+    }
+}
